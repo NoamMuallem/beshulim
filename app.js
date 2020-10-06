13a696d578a2ec4f8cbfc714a9fe07cc69cea167
Noam Muallem <noammu072@gmail.com>
2020-10-06 10:15:36 +0300

    fixing security issiue when pushing to heroku

--- app.js
+++ app.js
@@ -18,7 +18,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
 app.use(morgan("dev"));
 
 // DB Config
